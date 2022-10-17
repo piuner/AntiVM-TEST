59a7ecb9b8dc61df5fd3817fee7a71de622642c8
piuner <112301195+piuner@users.noreply.github.com>
2022-10-17 10:21:48 -0700

    Add files via upload

---
+++ AntiVM/Program.cs
@@ -0,0 +1,13 @@
+﻿using 反分析;
+
+namespace AntiVM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AntiVM
+            反分析黑鬼.運行反分析();
+        }
+    }
+}
